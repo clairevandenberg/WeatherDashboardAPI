97560ab16546f39c75d0f9d2a8fb79487157f7f1
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-27 12:58:26 +1030

    API Data & search bar

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=2a41be6b56e8918bc7efe98c840f4638";
+//search bar
+$("#searchBar").on("submit", function () {
+    event.preventDefault ();
+    getOpenWeather($("#search").val());
+});
 
+//Fetching API data on weatherHistory
+function getOpenWeather (weatherPerCity) {
+let  cityName = weatherPerCity;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=2a41be6b56e8918bc7efe98c840f4638";
+$.ajax ({
+url: queryUrl,
+method: "GET"
+.done (function (response) {
+    console.log(repsonse);
+    console.log(response-Runtime);
+    weatherHistory(cityName, reponse);
+})
+})
+}
 
