4b8d3a2a79fdf4d5fba2a0f062b1f0904c7c895e
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-04-01 22:51:10 +1030

    debugging & editing code

--- script.js
+++ script.js
@@ -2,42 +2,56 @@
 // @ts-checkl
 
 let inputEl = document.querySelector("#searchInput");
-let buttonEl = document.querySelector(".button")
+// let buttonEl = document.querySelector(".button");
 
 // DEBUG, DELETE ME LATER.
 getOpenWeather("Adelaide");
-date("current date");
+// dates("currentdate");
 
-//search bar
-$("#searchBar").on("submit", function (e) {
-    e.preventDefault ();
-    getOpenWeather(inputEl.value);
-    return getOpenWeather
-    }); 
+function buttonSearches() {
+let searchBar = localStorage.getItem("searchBtn");
+console.log(searchBar)
 
-// Local Storage Searched cities
-//call functuion 
-renderLastRegistered ();
+// if (searchBar === null) {
+//     return; 
+// }
 
- // fucntion 
-function renderLastRegistered() {
-var searchBar = localStorage.getItem("searchBtn");
-
-if (searchBar === null) {
-    return; 
+    localStorage.setItem("searchBar", Search);
+        buttonSearches ();
 }
 
-searchBar.textContent = city;
+//search bar
+$("#submitBtn").on("submit", function (e) {
+    e.preventDefault ();
 
-searchBar.addEventListener("click", function(event){
-    event.preventDefault();
+    //Gets what's typed in
+    var searchBar = inputEl.value;
+    
+    //API call with what's typed in
+    getOpenWeather(searchBar);
+});
 
-var searchBar = document.querySelector("#searchBtn").value;
+// Save User Searches to Local Storage 
+//call functuion to 
+// buttonSearches ();
 
-localStorage.setItem("searchBar", Search);
-renderLastRegistered ();
-})
-};
+// searchBar.addEventListener("click", function(event){
+// event.preventDefault();
+// var searchBar = document.querySelector("#searchBtn").value;
+
+//  // Search saved to Local Storage
+// function buttonSearches() {
+// let searchBar = localStorage.getItem("searchBtn");
+// console.log(searchBar)
+
+// // if (searchBar === null) {
+// //     return; 
+// // }
+
+// localStorage.setItem("searchBar", Search);
+// buttonSearches ();
+// }
+// });
 
 
 const queryAPI = () => {
@@ -47,7 +61,7 @@ const queryAPI = () => {
 
 //Fetching API data on weatherHistory
 function getOpenWeather (cityName) {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=fe991547b0b791499b6ebb2c115c9743";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=fe991547b0b791499b6ebb2c115c9743&units=metric";
     console.log(queryURL);
     console.log("Test");
 
@@ -61,13 +75,15 @@ function getOpenWeather (cityName) {
     })
     .then(current => {
         // current weather data
-        console.log(current)
-        console.log(current.id)
+        console.log(current);
+        console.log(current.id);
+
+        fillPageCurent(current);
         
         // get forecast by city-id
         return $.ajax({
             type: 'GET',
-            url: "api.openweathermap.org/data/2.5/forecast?" + cityName + "&appid=fe991547b0b791499b6ebb2c115c9743"
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=fe991547b0b791499b6ebb2c115c9743&units=metric"
         })
     })
     .then(forecast => {
@@ -76,36 +92,37 @@ function getOpenWeather (cityName) {
         // for each day
         // forecast.list[0].dt
         let date = moment.utc(forecast.list[0].dt * 1000);
-        console.log(date.format("DDD MMM Y"));
+        console.log(date.format("DD MMM Y"));
         
-        // current weather data.
+        // current weather data
         console.log(forecast);
         console.log(forecast.city.coord);
+
+        fillPageForecast(forecast);
+        
         
         // ADELAIDE {lon: 138.6, lat: -34.93}
         
         // Request UV forcast for 5 days by lat/lon
         return $.ajax({
             type: 'GET',
-            url: "http://api.openweathermap.org/data/2.5/uvi/forecast?appid={appid}&lat={lat}&lon={lon}&cnt={cnt}" + forecast.city.coord.lat,
+            url: `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=fe991547b0b791499b6ebb2c115c9743&lat=${forecast.city.coord.lat}&lon=${forecast.city.coord.lon}`
         });
-    })
-
-    .then(uv => {
-    // uv data
+    }).then(uv => {
     console.log(uv);
     })
     
     //store data in an object 
-    .then(function(response) {
+    .then(function(forecast) {
+        console.log(forecast);
     console.log(queryURL);
     console.log(response);
 
     
-    // Displaying data retreived from Open Weather API to HTML
-    $(".cityName").html("<h1>" + response.name + date);
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
+    // // Displaying data retreived from Open Weather API to HTML
+    // $(".cityName").html("<h1>" + response.name + date);
+    // $(".wind").text("Wind Speed: " + response.wind.speed);
+    // $(".humidity").text("Humidity: " + response.main.humidity);
 
     // transfer to fahrenheit
  var temperatureF = (response.main.temp -273.15) * 1.80 + 32;
@@ -119,14 +136,49 @@ function getOpenWeather (cityName) {
 function dates () {
     // today 
     var today = moment().format('dddd, MMMM Do YYYY, h:mm a')
-    console.log(date.format("DDD MMM Y"));
+    console.log(date.format("DD MMM Y"));
     //loop for 5 days
 for (let i = 0; i > 4 ; i++) {
-    fiveDays [i] = moment().add(i).format ('DDD MMM Y')
+    fiveDays [i] = moment().add(i).format ('DD MMM Y')
     $(".date" + i).text('cards${i}');
     console.log()
 }
 }
+}
+
+function fillPageCurent (current) {
+ // Displaying data retreived from Open Weather API to HTML
+ let date = moment.utc(current.dt * 1000);
+        console.log(date.format("DD MMM Y"));
+        
+ $(".cityName").html("<h1>" + current.name + date.format(" DD MMM Y"));
+ $(".wind").text("Wind Speed: " + current.wind.speed);
+ $(".humidity").text("Humidity: " + current.main.humidity);
+ $(".temperatureF").text("Temperature: " + current.main.temp);
+
+}
+
+function fillPageForecast(forecast) {
+
+    
+var savedWeather = (forecast.list);
+console.log(savedWeather);
+for (let i = 0; i < 5 ; i++) {
+    //saving time into variable 
+    let forecastDate = moment().add(i);
+
+
+    
+let dateEl= document.getElementById(`dateEl${i}`);
+let temperature= document.getElementById(`temperature${i}`);
+let humidity= document.getElementById(`humidity${i}`);
+
+$(".date0").text("<h1>" + date.format(" DD MMM Y"));
+$(".temperature0").text("Humidity: " + current.main.humidity);
+$(".humidity0").text("Temperature: " + current.main.temp);
+}
+}
+
 
 // displaying Weather on todays weather 
 // var cityForecast = response(); 
